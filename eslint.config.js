59ac7f843899d69fd78533b723e40f0b1bbd384c
Doran Urban <kontakt@doranurban.de>
2025-02-07 10:44:29 +0100

    debug github action

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
 // eslint.config.js
-module.exports = {
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 2020,
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+    environment: {
+      browser: true,
+      node: true,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+    },
   },
-  env: {
-    browser: true,
-    node: true,
-  },
-  rules: {
-    "no-console": "warn",
-    "no-unused-vars": "warn",
-  },
-};
+];
