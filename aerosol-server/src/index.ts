93799545ea75197fb334487da54fcc33cb221bf5
Doran Urban <kontakt@doranurban.de>
2025-02-12 10:36:07 +0100

    feat env for file size limit

--- aerosol-server/src/index.ts
+++ aerosol-server/src/index.ts
@@ -22,8 +22,8 @@ app.use(
   })
 );
 
-app.use(express.urlencoded({ limit: '50mb' }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: process.env.FILE_UPLOAD_LIMIT }));
+app.use(express.json({ limit: process.env.FILE_UPLOAD_LIMIT }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
